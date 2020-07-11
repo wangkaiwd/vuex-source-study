47f5e734211fd9cff791f064aed1483c4e079822
wangkaiwd <1162486176@qq.com>
2020-07-11 17:36:02 +0800

    feat: webpack output and entry config options study

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -3,10 +3,39 @@ const path = require('path');
 const webpack = require('webpack');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
+// bundle: 从许多不同的模块产生，`bundles`包含已经经历过加载和编译处理的源文件的最终版本
+// chunk: 这是webpack特定的术语，被用来从内部去管理捆绑过程。`Bundles`由如`entry`和`child`这些类型的`chunks`组成。
+//        通常，`chunks`直接对应于输出`bundle`，然后，有一些不会产生一对一关系的配置
 module.exports = {
   mode: 'development',
 
-  // 这里用reduce的原因：
+  // 这里用reduce的原因：将一个数组最终处理为一个对象
+  // 这种将数组最终整合成一个元素的逻辑，可以使用reduce来进行处理
+
+  // 最终输出：
+  // entries {
+  //   chat: [
+  //     'webpack-hot-middleware/client',
+  //     '/Users/wangkai/workSpace/personalCode/study01/vueJs/vuex-source-study/examples/chat/app.js'
+  //   ],
+  //   counter: [
+  //     'webpack-hot-middleware/client',
+  //     '/Users/wangkai/workSpace/personalCode/study01/vueJs/vuex-source-study/examples/counter/app.js'
+  //   ],
+  //   'counter-hot': [
+  //     'webpack-hot-middleware/client',
+  //     '/Users/wangkai/workSpace/personalCode/study01/vueJs/vuex-source-study/examples/counter-hot/app.js'
+  //   ],
+  //   'shopping-cart': [
+  //     'webpack-hot-middleware/client',
+  //     '/Users/wangkai/workSpace/personalCode/study01/vueJs/vuex-source-study/examples/shopping-cart/app.js'
+  //   ],
+  //   todomvc: [
+  //     'webpack-hot-middleware/client',
+  //     '/Users/wangkai/workSpace/personalCode/study01/vueJs/vuex-source-study/examples/todomvc/app.js'
+  //   ]
+  // }
+  // entries的key是chunk的名字，值描述了chunk的入口点
   entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
     const fullDir = path.join(__dirname, dir);
     const entry = path.join(fullDir, 'app.js');
@@ -15,12 +44,11 @@ module.exports = {
     }
     return entries;
   }, {}),
-
   output: {
-    path: path.join(__dirname, '__build__'),
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js',
-    publicPath: '/__build__/'
+    path: path.join(__dirname, '__build__'), // bundle写入path指定的目录，需要使用绝对路径
+    filename: '[name].js', // 决定每个输出bundle的名字的选项
+    chunkFilename: '[id].chunk.js', // 非入口chunk文件的名字
+    publicPath: '/__build__/', // 1. 加载静态资源 2. cdn  值为运行时或loader创建的每个url前缀
   },
 
   module: {
---
+++ thinking.md
@@ -0,0 +1,26 @@
+## 阅读思路
+
+### `readme.md`文档
+文档中主要介绍了使用`Vuex`实现的几个例子，以及运行项目的方法。
+
+* [`Examples`](https://github.com/vuejs/vuex#examples)
+
+### `package.json`文件
+
+* `npm dev`做了什么
+* `server.js`的代码怎样编写？
+  * 通过文档查看2个库的用法
+    * webpack-dev-middleware
+    * [Using webpack-dev-middleware](https://v4.webpack.js.org/guides/development/#using-webpack-dev-middleware)
+    * webpack-hot-middleware
+  * 学习`webpack`的配置项：
+    * [`glossary`](https://v4.webpack.js.org/glossary/)
+* `vuex`的`demo`搭建思路
+
+#### 挑战
+> 尝试了解下面的知识，为自己开发库做准备
+
+* `git`提交规范如何控制
+* 如何进行版本管理
+  * release
+  * changelog
